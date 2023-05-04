d4bb05ab2db37fb3b9046ca96597d6d3fa804c0e
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-04 12:59:48 +0500

    Update Evgeny.py

--- Evgeny.py
+++ Evgeny.py
@@ -3,20 +3,21 @@ import pandas as pd
 import csv
 import time
 
-df = {}
-List = ''
-warning = ''
-titanic_data_1 = {}
-titanic_data_2 = {}
-titanic_data_3 = {}
-titanic_data_4 = {}
+def Evgeny_Function():
+ df = {}
+ List = ''
+ warning = ''
+ titanic_data_1 = {}
+ titanic_data_2 = {}
+ titanic_data_3 = {}
+ titanic_data_4 = {}
 
-st.title('Hey, it is my project TITANIC')
-st.divider()
-st.subheader('My name: Evgeny. My group: 2022-FGiIB-PI-2SM ')
-st.success('Вариант-6. Вывести Name, Sex, Age спасенных пассижиров указанного класса и пола.')
+ st.title('Hey, it is my project TITANIC')
+ st.divider()
+ st.subheader('My name: Evgeny. My group: 2022-FGiIB-PI-2SM ')
+ st.success('Вариант-6. Вывести Name, Sex, Age спасенных пассижиров указанного класса и пола.')
 
-with st.sidebar:
+ with st.sidebar:
     add_multiselect1 = st.multiselect(
         'Выберите класс пассажира',
         ['1', '2', '3'])
@@ -28,33 +29,33 @@ with st.sidebar:
     add_button = st.button('показать')
 
 if add_button:
-    if not add_multiselect1 or not add_multiselect2:
-        st.error('Заданы не все данные условия!')
-    else:
-        with st.spinner('Загрузка...'):
-            time.sleep(5)
-            st.success('Данные загружены!')
-            count = 0
-            count2 = 1
+     if not add_multiselect1 or not add_multiselect2:
+         st.error('Заданы не все данные условия!')
+     else:
+         with st.spinner('Загрузка...'):
+             time.sleep(5)
+             st.success('Данные загружены!')
+             count = 0
+             count2 = 1
 
-        with open('data.csv', encoding='utf-8') as result_file:
-            file_reader = csv.reader(result_file, delimiter = ",")
-            for row in file_reader:
-                if row[1] == '1' and row[2] in add_multiselect1 and row[4] in add_multiselect2:
-                    titanic_data_4[count2] = str(f'{row[2]}')
-                    titanic_data_1[count2] = str(f'{row[3]}')
-                    titanic_data_2[count2] = str(f'{row[4]}')
-                    titanic_data_3[count2] = str(f'{row[5]}')
-                    count2 += 1
-            count += 1
-else:
-    data = pd.read_csv('data.csv')
-    st.write(data)
+         with open('data.csv', encoding='utf-8') as result_file:
+             file_reader = csv.reader(result_file, delimiter = ",")
+             for row in file_reader:
+                 if row[1] == '1' and row[2] in add_multiselect1 and row[4] in add_multiselect2:
+                     titanic_data_4[count2] = str(f'{row[2]}')
+                     titanic_data_1[count2] = str(f'{row[3]}')
+                     titanic_data_2[count2] = str(f'{row[4]}')
+                     titanic_data_3[count2] = str(f'{row[5]}')
+                     count2 += 1
+             count += 1
+ else:
+     data = pd.read_csv('data.csv')
+     st.write(data)
 
-df = pd.DataFrame({
-        "Pclass": titanic_data_4,
-        "Name": titanic_data_1,
-        "Sex": titanic_data_2,
-        "Age": titanic_data_3, 
-    })
-st.dataframe(df)
+ df = pd.DataFrame({
+         "Pclass": titanic_data_4,
+         "Name": titanic_data_1,
+         "Sex": titanic_data_2,
+         "Age": titanic_data_3, 
+     })
+ st.dataframe(df)
