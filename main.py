c7b9d2fcb8c013a1c20a2414f619407d39e92d13
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-04 12:51:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import csv
+from Evgeny import *
+from Kate import *
+from Alina import *
+from Vadim import *
+
+st.title('STREAMLIT PROJECT КОМАНДЫ 2022-ФГиИБ-ПИ-2см')
+st.divider()
+st.subheader('Выберите нужный вариант: ')
+
+select = st.radio('Выберите исполнителя:', ['Екатерина', 'Алина', 'Вадим', 'Евгений'])
+if select == 'Екатерина':
+    Kate_Function()
+if select == 'Алина':
+    Alina_Function()
+if select == 'Вадим':
+    Vadim_Function()
+if select == 'Евгений':
+    Evgeny_Function()
+
