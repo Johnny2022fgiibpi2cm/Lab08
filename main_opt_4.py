ae8cf43a1f98067c732267a63481f752f804e3ad
Alina <alina.eliseeva2023@mail.ru>
2023-05-20 15:36:27 +0500

    Алина, вариант 4

---
+++ main_opt_4.py
@@ -0,0 +1,17 @@
+def get_fare(data):
+ choice = 32
+ res = []
+ for line in data:
+      if line.split(",")[10] != 'Cabin':
+            fare = float(line.split(",")[10])
+            name = line.split(",")[3] + line.split(",")[4]
+            if fare >= choice:
+                res.append(name[1:-1])
+ return res
+
+with open("data.csv") as file:
+ data = file.readlines()
+print(get_fare(data))
+
+
+
