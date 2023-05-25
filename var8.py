f3a460231c3f53803bedf6bc64bac3b54a57132b
Ekaterina Lenskaya <kate.noskova@gmail.com>
2023-05-25 21:41:58 +0500

    Екатерина вариант 8

--- var8.py
+++ var8.py
@@ -1,25 +1,33 @@
 import streamlit as st
 import csv
 
-male_saved = 0
-male_dead = 0
-female_saved = 0
-female_dead = 0
-
-with open('data.csv') as f:
-    reader = csv.DictReader(f)
-    for line in reader:
-        if line['Sex'] == 'male':
-            if line['Survived'] == '1':
+#Функция не зависит от файла Титаник
+def get_result(data):
+    male_saved = 0
+    male_dead = 0
+    female_saved = 0
+    female_dead = 0
+    for line in data:
+        if line.split(',')[5] == 'male':
+            if line.split(',')[1] == '1':
                 male_saved += 1
             else:
                 male_dead += 1
-        elif line['Sex'] == 'female':
-            if line['Survived'] == '1':
+        elif line.split(',')[5] == 'female':
+            if line.split(',')[1] == '1':
                 female_saved += 1
             else:
                 female_dead += 1
+    return male_saved, male_dead, female_saved, female_dead
 
+#Вывели файл Титаник из функции
+with open('data.csv') as file:
+    data = file.readlines()
+
+male_saved, male_dead, female_saved, female_dead = get_result(data)
+#print(male_saved, male_dead, female_saved, female_dead)
+
+#Программа для Стримлит на основе полученных данных
 st.title('Спасенные и погибшие на Титанике')
 st.header('Вариант 8 (Ленская Е.И. группа ПИ-2см)')
 st.subheader('Посчитать количество пассажиров (выбрав пол, и спасенных или погибших)')
