b00315d1c725b1b5b939636bed84309630e32ef7
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-26 14:58:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
 from Evgeny import *
 from Evgeny_Test import *
 #from Kate import *
-#from Alina import *
+from Alina import *
 from Vadim import *
 
 st.title('STREAMLIT PROJECT КОМАНДЫ 2022-ФГиИБ-ПИ-2см')
@@ -14,8 +14,8 @@ st.subheader('Выберите нужный вариант: ')
 select = st.radio('Выберите исполнителя:', ['Екатерина', 'Алина', 'Вадим', 'Евгений'])
 #if select == 'Екатерина':
 #    Kate_Function()
-#if select == 'Алина':
-#    Alina_Function()
+if select == 'Алина':
+    Alina_Function()
 if select == 'Вадим':
     Vadim_Function()
 if select == 'Евгений':
