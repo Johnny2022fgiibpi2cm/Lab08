b04d6f6dffff2a25d935541f6183e6f18521df0e
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-26 15:55:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ st.subheader('Выберите нужный вариант: ')
 select = st.radio('Выберите исполнителя:', ['Екатерина', 'Алина', 'Вадим', 'Евгений'])
 if select == 'Екатерина':
     male_saved, male_dead, female_saved, female_dead = get_result(data)
+    #Программа для Стримлит на основе полученных данных
+st.title('Спасенные и погибшие на Титанике')
+st.header('Вариант 8 (Ленская Е.И. группа ПИ-2см)')
+st.subheader('Посчитать количество пассажиров (выбрав пол, и спасенных или погибших)')
+choice = st.radio('Укажите пол:', ['муж', 'жен'])
+if choice == 'муж':
+    choice = st.radio('Спасен или погиб:', ['спасен', 'погиб'])
+    if choice == 'спасен':
+        st.success(f"Количество спасенных мужчин - {male_saved}")
+    else:
+        st.error(f"Количество погибших мужчин - {male_dead}")
+else:
+    choice = st.radio('Спасен или погиб:', ['спасен', 'погиб'])
+    if choice == 'спасен':
+        st.success(f"Количество спасенных женщин - {female_saved}")
+    else:
+        st.error(f"Количество погибших женщин - {female_dead}")
 if select == 'Алина':
     test_fare()
     Alina_Function()
