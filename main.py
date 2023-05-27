1289c4a19432d0a92e5475b824baeea734281f6e
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-27 13:44:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import csv
 import time
-from Variant_6 import *
+#from Variant_6 import *
 from Variant_6_test import *
 from Variant_8 import *
 from Variant_8_test import *
