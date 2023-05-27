61e3393650f152e53599ef782ccebbf79bb05c46
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-27 10:08:27 +0500

    Update Variant_4.py

--- Variant_4.py
+++ Variant_4.py
@@ -1,19 +1,3 @@
-import streamlit as st
-
-def Alina_Function():
- st.title('Титаник')
- st.header('Вариант 4 (Елисеева А.А. группа ПИ-2см)')
- st.subheader('Вывести имена пассажиров, стоимость билета которых была выше указанной')
- choice = st.number_input('Укажите стоимость билета')
- if st.button('Показать'):
-    with open("data.csv") as file:
-     for line in file:
-         if line.split(",")[10] != 'Cabin':
-             fare = float(line.split(",")[10])
-             name = line.split(",")[3] + line.split(",")[4]
-             if fare >= choice:
-               st.text(f"{name[1:-1]}{fare}")
-
 def get_fare(data):
  choice = 32
  res = []
