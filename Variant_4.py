9fad6d352f4282e94d4071dc98303286df3dfbcb
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-27 09:21:10 +0500

    Update Variant_4.py

--- Variant_4.py
+++ Variant_4.py
@@ -29,10 +29,6 @@ with open("data.csv") as file:
  data = file.readlines()
 print(get_fare(data))
 
-def test_fare():
- data = ['0,1,2,.Грейнджер, Гермиона.,5,6,7,8,9,56', '0,1,2,/Поттер, Гарри.,5,6,7,8,9,5', '0,1,2,-Уизли, Рон.,5,6,7,8,9,31']
- assert get_fare(data) == ['Грейнджер Гермиона']
-
 
 
 
