4dea39228310ffb94fe694cbbb0c36247dd119cc
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-27 12:35:59 +0500

    Update Variant_8_.py

--- Variant_8_.py
+++ Variant_8_.py
@@ -1,7 +1,3 @@
-import streamlit as st
-import csv
-
-#Функция не зависит от файла Титаник
 def get_result(data):
     male_saved = 0
     male_dead = 0
@@ -23,24 +19,3 @@ def get_result(data):
 #Вывели файл Титаник из функции
 with open('data.csv') as file:
     data = file.readlines()
-
-male_saved, male_dead, female_saved, female_dead = get_result(data)
-#print(male_saved, male_dead, female_saved, female_dead)
-
-#Программа для Стримлит на основе полученных данных
-st.title('Спасенные и погибшие на Титанике')
-st.header('Вариант 8 (Ленская Е.И. группа ПИ-2см)')
-st.subheader('Посчитать количество пассажиров (выбрав пол, и спасенных или погибших)')
-choice = st.radio('Укажите пол:', ['муж', 'жен'])
-if choice == 'муж':
-    choice = st.radio('Спасен или погиб:', ['спасен', 'погиб'])
-    if choice == 'спасен':
-        st.success(f"Количество спасенных мужчин - {male_saved}")
-    else:
-        st.error(f"Количество погибших мужчин - {male_dead}")
-else:
-    choice = st.radio('Спасен или погиб:', ['спасен', 'погиб'])
-    if choice == 'спасен':
-        st.success(f"Количество спасенных женщин - {female_saved}")
-    else:
-        st.error(f"Количество погибших женщин - {female_dead}")
