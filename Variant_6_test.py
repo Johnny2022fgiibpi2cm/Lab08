1778bd5471d847ed9de3fddc1d94c004eb6e2ee9
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-29 11:13:51 +0500

    Update Variant_6_test.py

--- Variant_6_test.py
+++ Variant_6_test.py
@@ -1,13 +1,13 @@
 from Variant_6 import Result_Function
 
-'''#Test Survived = 1, Pclass = 2, Sex = male
+#Test Survived = 1, Pclass = 2, Sex = male
 def Testing_Function_1():
     test = ['0, 1, 2, "Williams Mr.Charles Eugene", male, ',
             '1, 1, 2, "Beesley Mr.Lawrence", male, 34',
             '2, 1, 2, "Becker Master.Richard F", male, 1']
     answer = Result_Function(test, '1', '2', 'male', '0', '0')
     assert answer == ['"Williams Mr.Charles Eugene", male, ', '"Beesley Mr.Lawrence", male, 34', '"Becker Master.Richard F", male, 1']
-
+'''
 #Test Survived = 1, Pclass = 1, Sex = female
 def Testing_Function_2():
     test = ['0, 1, 1, "Bonnell Miss.Elizabeth", female, 58',
