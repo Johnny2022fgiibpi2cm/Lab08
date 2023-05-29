fbe3fffce78e01c9cd4ab081cbad51c783c84bf6
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-29 09:46:05 +0500

    Update Variant_6_test.py

--- Variant_6_test.py
+++ Variant_6_test.py
@@ -5,11 +5,11 @@ def Testing_Function_1():
     test = ['0, 1, 2, "Williams Mr.Charles Eugene", male, ',
             '1, 1, 2, "Beesley Mr.Lawrence", male, 34',
             '2, 1, 2, "Becker Master.Richard F", male, 1']
+     a = ['1', '1', '1']
+     assert a == ['1', '1', '1']
     #assert Result_Function(test, '1', '2', 'male', '0', '0') == ['"Williams Mr.Charles Eugene", male, ',
                                                        '"Beesley Mr.Lawrence", male, 34',
                                                        '"Becker Master.Richard F", male, 1']
-     a = ['1', '1', '1']
-     assert a == ['1', '1', '1']
 
 #Test Survived = 1, Pclass = 1, Sex = female
 def Testing_Function_2():
