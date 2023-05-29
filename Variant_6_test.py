b60b27d3532cc6550768e3bd97ccf82cefb3bea5
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-29 09:57:44 +0500

    Update Variant_6_test.py

--- Variant_6_test.py
+++ Variant_6_test.py
@@ -5,9 +5,7 @@ def Testing_Function_1():
     test = ['0, 1, 2, "Williams Mr.Charles Eugene", male, ',
             '1, 1, 2, "Beesley Mr.Lawrence", male, 34',
             '2, 1, 2, "Becker Master.Richard F", male, 1']
-     a = 3
-     assert a == 3
-    #assert Result_Function(test, '1', '2', 'male', '0', '0') == ['"Williams Mr.Charles Eugene", male, ',
+    assert Result_Function(test, '1', '2', 'male', '0', '0') == ['"Williams Mr.Charles Eugene", male, ',
                                                        '"Beesley Mr.Lawrence", male, 34',
                                                        '"Becker Master.Richard F", male, 1']
 
