7acc14f77c0ec56b058706df973b095293d86815
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-05-29 11:42:41 +0500

    Update Variant_6_test.py

--- Variant_6_test.py
+++ Variant_6_test.py
@@ -1,14 +1,5 @@
 from Variant_6 import Result_Function
-import pytest
 
-@pytest.fixture()
-def some_data():
-    return 42
-
-def test_some_data(some_data):
-    assert some_data == 42
-
-'''
 #Test Survived = 1, Pclass = 2, Sex = male
 def Testing_Function_1():
 
@@ -16,8 +7,8 @@ def Testing_Function_1():
             '1, 1, 2, "Beesley Mr.Lawrence", male, 34',
             '2, 1, 2, "Becker Master.Richard F", male, 1']
     answer = Result_Function(test, '1', '2', 'male', '0', '0')
-    #assert answer == ['"Williams Mr.Charles Eugene", male, ', '"Beesley Mr.Lawrence", male, 34', '"Becker Master.Richard F", male, 1']
-   
+    assert answer == ['"Williams Mr.Charles Eugene", male, ', '"Beesley Mr.Lawrence", male, 34', '"Becker Master.Richard F", male, 1']
+  ''' 
 
 #Test Survived = 1, Pclass = 1, Sex = female
 def Testing_Function_2():
