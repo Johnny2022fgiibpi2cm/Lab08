566089b289562e451c6a7a8fcc75cd70cf022f6a
Ekaterina Lenskaya <kate.noskova@gmail.com>
2023-05-31 08:05:07 +0500

    Екатерина 8 вариант

---
+++ Variat_8.py
@@ -0,0 +1,17 @@
+def get_result(data):
+    male_saved = 0
+    male_dead = 0
+    female_saved = 0
+    female_dead = 0
+    for line in data:
+        if line.split(',')[5] == 'male':
+            if line.split(',')[1] == '1':
+                male_saved += 1
+            else:
+                male_dead += 1
+        elif line.split(',')[5] == 'female':
+            if line.split(',')[1] == '1':
+                female_saved += 1
+            else:
+                female_dead += 1
+    return male_saved, male_dead, female_saved, female_dead
