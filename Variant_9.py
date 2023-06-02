722ff1b95a32bfaad72e8b1df24249ab558ede7a
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-06-02 12:17:38 +0500

    Update Variant_9.py

--- Variant_9.py
+++ Variant_9.py
@@ -4,5 +4,7 @@ def saved_pass(data):
     for string in data:
         if string.split(',')[1] == '1' and string.split(',')[6] < '30': 
             saved_30 += 1
+        elif string.split(',')[1] == '1' and string.split(',')[6] > '60':
+            saved_60 += 1
     return saved_30, saved_60
 
