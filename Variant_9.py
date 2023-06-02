42e8b8b207aee4935f729b4d5667af1f7bcc0ed7
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-06-02 12:37:35 +0500

    Update Variant_9.py

--- Variant_9.py
+++ Variant_9.py
@@ -1,10 +1,16 @@
 def saved_pass(data):
     saved_30 = 0
+    count_30 = 0
     saved_60 = 0
+    count_60 = 0
     for string in data:
-        if string.split(',')[1] == '1' and string.split(',')[6] < '30': 
-            saved_30 += 1
-        elif string.split(',')[1] == '1' and string.split(',')[6] > '60':
-            saved_60 += 1
-    return saved_30, saved_60
+        if string.split(',')[6] < '30':
+            count_30 += 1
+            if string.split(',')[1] == '1':
+                saved_30 += 1
+        elif string.split(',')[6] > '60':
+            count_60 += 1
+            if string.split(',')[1] == '1':
+                saved_60 += 1
+    return saved_30, saved_60, count_30, count_60
 
