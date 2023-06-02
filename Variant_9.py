f28e74ade12c21e030c02a00d7f1e8bcf05a3646
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-06-02 11:51:26 +0500

    Update Variant_9.py

--- Variant_9.py
+++ Variant_9.py
@@ -1,8 +1,5 @@
 def saved_pass(data):
-    saved_male_30 = 0
-    saved_female_30 = 0
-    saved_male_60 = 0
-    saved_female_60 = 0
+    saved_male_30 = 0, saved_female_30 = 0, saved_male_60 = 0, saved_female_60 = 0
     #for line in data:
     #    if line.split(',')[5] == 'male':
     #        if line.split(',')[1] == '1':
@@ -14,5 +11,5 @@ def saved_pass(data):
     #            female_saved += 1
     #        else:
     #            female_dead += 1
-    return male_saved, male_dead, female_saved, female_dead
+    return saved_male_30
 
