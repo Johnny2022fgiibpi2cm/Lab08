10fbab140798c1b98479d220eb5a56da4b3cc7fa
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-06-02 12:54:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if select == 'Вадим':
     saved_30, saved_60, count_30, count_60 = saved_pass(data)
     if choice == 'до 30 лет':
         st.success(f"Всего пассажиров: {count_30}, Всего выживших пассажиров: {saved_30}")
-        result_30 = count_30 * saved_30/ 100 
+        result_30 =  saved_30 * 100 / count_30 
         st.success(f"Доля выживших пассажиров: {result_30}")
     elif choice == 'старше 60 лет':
         st.success(f"Всего пассажиров: {count_60}, Всего выживших пассажиров: {saved_60}")
