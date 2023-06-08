6060f94785faed8c324be45c48f7d052c7754a0f
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-06-08 15:46:25 +0500

    Update Variant_9_test.py

--- Variant_9_test.py
+++ Variant_9_test.py
@@ -4,4 +4,9 @@ def test_1():
     data = ['1,1,2,3,4,male,27', '2,1,2,3,3,female,17', '3,1,2,3,4,female,33', '4,1,2,3,4,male,29']
     saved_30, saved_60, count_30, count_60 = saved_pass(data)
     assert saved_30 == 3
+    
+def test_2():
+    data = ['1,1,2,3,4,male,27', '2,1,2,3,3,female,17', '3,1,2,3,4,female,33', '4,1,2,3,4,male,77']
+    saved_30, saved_60, count_30, count_60 = saved_pass(data)
+    assert saved_60 == 1
 
