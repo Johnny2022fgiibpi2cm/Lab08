e75322631e291744d1db82d069f61ee8d8eba29b
Johnny2022fgiibpi2cm <128688315+Johnny2022fgiibpi2cm@users.noreply.github.com>
2023-06-08 15:37:03 +0500

    Update Variant_9_test.py

--- Variant_9_test.py
+++ Variant_9_test.py
@@ -2,6 +2,6 @@ from Variant_9 import saved_pass
  
 def test_1():
     data = ['0,0,2,3,4,male', '0,0,2,3,4,male', '0,1,2,3,4,female', '0,1,2,3,4,female']
-    male_saved, male_dead, female_saved, female_dead = get_result(data)
+    saved_30, saved_60, count_30, count_60 = saved_pass(data)
     assert male_saved == 0
 
